1ba2cc9edf41c5160ac12636844c84dcb64ede13
Paul Bakker <paul@paulgbakker.com>
2016-04-20 12:44:08 +0200

    Add onsolutionclose event handler with code to store Code Coverage data

---
+++ JenkinsDemo_test/JenkinsDemo_test.js
@@ -0,0 +1,16 @@
+
+/**
+ * Callback method for when solution is closed, force boolean argument tells if this is a force (not stoppable) close or not.
+ *
+ * @param {Boolean} force if false then solution close can be stopped by returning false
+ *
+ * @returns {Boolean}
+ *
+ * @properties={typeid:24,uuid:"C7C3F247-0DF6-4EF0-A4F7-B785B53811AF"}
+ */
+function onSolutionClose(force) {
+	if (scopes['codeCoverageReporting']) {
+		scopes['codeCoverageReporting'].storeCoverageData()
+	}
+	return true
+}
